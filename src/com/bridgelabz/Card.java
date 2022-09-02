573ee7026133f2e98a3bfd96de7ce8fef99d8f55
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-02 21:25:21 +0530

    ADDED: create the number of card and shuffle the deck

--- src/com/bridgelabz/Card.java
+++ src/com/bridgelabz/Card.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
 public class Card {
-    public static void main(String[] args) {
-        System.out.println("Welcome to card game");
+    public String[] suits = {"clubs","Diamonds","Hearts","Spades"};
+    public String[] ranks = {"2","3","4","5","6","7","8","9","10","jack","Queen","king","ace"};
+    public String[][] arrayOfCard = new String[4][13];
+    int numberOfCard = suits.length * ranks.length;
+    public String[] deck = new String[numberOfCard];
+
+
+    public void deck(){
+        for (int i = 0; i < numberOfCard; i++) {
+            int random = i +(int)(Math.random()*(numberOfCard-i));
+            String temp = deck[random];
+            deck[random] = deck[i];
+            deck[i] = temp;
+        }
     }
 }
---
+++ src/com/bridgelabz/CardMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+
+public class CardMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to card game");
+        Card card = new Card();
+        card.deck();
+    }
+}
